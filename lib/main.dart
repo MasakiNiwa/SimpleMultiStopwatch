fc4bcb67f10e5754fb4061cb50e7b84456553e64
masaki.niwa <masaki-dev@rakumail.jp>
2024-05-04 13:27:13 +0900

    タイマーwidgetを外部ファイル化

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:simple_multi_stopwatch/focus_timer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,79 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton:
           ElevatedButton(onPressed: addTimer, child: const Icon(Icons.add)),
-    );
-  }
-}
-
-class FocusTimer extends StatefulWidget {
-  const FocusTimer({super.key});
-
-  @override
-  State<FocusTimer> createState() => _FocusTimerState();
-}
-
-class _FocusTimerState extends State<FocusTimer> {
-  final stopwatch = Stopwatch();
-  String getTimeCount() {
-    return stopwatch.elapsed.inDays.toString().padLeft(2, '0') +
-        ":" +
-        (stopwatch.elapsed.inHours % 24).toString().padLeft(2, '0') +
-        ":" +
-        (stopwatch.elapsed.inMinutes % 60).toString().padLeft(2, '0') +
-        ":" +
-        (stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, '0') +
-        "." +
-        ((stopwatch.elapsed.inMilliseconds % 1000) ~/ 10)
-            .toString()
-            .padLeft(2, '0');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Timer.periodic(const Duration(milliseconds: 10), (Timer timer) {
-      setState(() {});
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.grey,
-          width: 1,
-        ),
-        borderRadius: BorderRadius.circular(7),
-        color: Colors.lightBlue[100],
-      ),
-      margin: const EdgeInsets.all(2),
-      padding: const EdgeInsets.all(5),
-      child: Row(
-        children: [
-          Text(
-            getTimeCount(),
-            style: const TextStyle(fontSize: 25),
-          ),
-          const Spacer(),
-          ElevatedButton(
-              onPressed: () {
-                stopwatch.start();
-              },
-              child: const Icon(Icons.timer)),
-          ElevatedButton(
-              onPressed: () {
-                stopwatch.stop();
-              },
-              child: const Icon(Icons.stop)),
-          ElevatedButton(
-              onPressed: () {
-                stopwatch.reset();
-              },
-              child: const Icon(Icons.restart_alt)),
-          const Spacer(),
-        ],
-      ),
     );
   }
 }
